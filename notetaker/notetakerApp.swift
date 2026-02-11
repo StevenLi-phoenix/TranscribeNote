ad52c0f7dbad58a4d03f9f7ead1e84c691563ff6
Steven Li <lishuyustevenli@163.com>
2026-02-11 18:13:25 -0500

    Initial Commit

---
+++ notetaker/notetakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  notetakerApp.swift
+//  notetaker
+//
+//  Created by Steven Li on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct notetakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
